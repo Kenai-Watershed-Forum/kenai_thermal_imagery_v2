1aa104a14632d311ac2adfe49310238be2370fed
benjamin-meyer-ak <69653328+benjamin-meyer-ak@users.noreply.github.com>
2022-12-22 11:36:51 -0900

    commit

--- script_to_publish.R
+++ script_to_publish.R
@@ -6,5 +6,18 @@
 # terminal command to stage all commits:
 # git add -A && git commit -m 'staging all files'
 
+# 12/21/2022
 
+# html render will not publish to url. checked versions of R, rstudio, and TeXworks
+# error message is fully unclear:
+
+# compilation failed- no matching packages
+#LaTeX Error: File `scrreprt.cls' not found.
+
+#Type X to quit or <RETURN> to proceed,
+#or enter new name. (Default extension: cls)
+
+# posted to SO
+
+# next step: start from scratch with a v3 and re-render piece by piece??
 
