63afbc703ad3f544432d15fb0ad7bc9e5ca870a4
benjamin-meyer-ak <69653328+benjamin-meyer-ak@users.noreply.github.com>
2022-12-21 18:04:54 -0900

    staging all files

--- script_to_publish.R
+++ script_to_publish.R
@@ -1,7 +1,10 @@
-# script to republish book
+# script to republish book at https://kenaiwatershed.quarto.pub/kenai_thermal_imagery/
 
 # in terminal:
 # quarto publish quarto-pub
 
 # terminal command to stage all commits:
 # git add -A && git commit -m 'staging all files'
+
+
+
